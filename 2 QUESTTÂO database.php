2c81d944823a30602b137800e56a9e4b0c4e7c91
alysson3xxx <alyssonsaraiva9@gmail.com>
2025-06-20 23:38:43 -0300

    2 QUESTTÂO database.php

---
+++ 2 QUESTTÂO database.php
@@ -0,0 +1,10 @@
+<?php
+$db = new SQLite3('tarefas.db');
+
+$db->exec("CREATE TABLE IF NOT EXISTS tarefas (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+descricao TEXT NOT NULL,
+vencimento TEXT NOT NULL,
+concluida INTEGER DEFAULT 0
+)");
+?>
